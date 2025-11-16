ee76003b431d0e60d4d4e86973e1eb7d30ec917e
cruelestpigeon <sketchbooktown2@gmail.com>
2025-11-16 01:53:44 -0500

    Delete storage.js

--- storage.js
+++
@@ -1,6 +0,0 @@
-export function loadData(){
- let d=localStorage.getItem('statusApp');
- if(d)return JSON.parse(d);
- return {posts:[],characters:[],lore:""};
-}
-export function saveData(d){localStorage.setItem('statusApp',JSON.stringify(d));}
